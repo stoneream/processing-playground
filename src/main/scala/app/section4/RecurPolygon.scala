8f346b098ac18aedf4c286cd268e6fd540fbbc03
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2023-03-05 14:20:55 +0900

    var使わずに済むようになったけど重いはず

--- src/main/scala/app/section4/RecurPolygon.scala
+++ src/main/scala/app/section4/RecurPolygon.scala
@@ -2,8 +2,6 @@ package app.section4
 
 import processing.core.{PApplet, PVector}
 
-import scala.util.Random
-
 class RecurPolygon extends PApplet {
   private val pad = 100
   private val w = 1000
@@ -12,11 +10,29 @@ class RecurPolygon extends PApplet {
   private val gap = 0.1f
   private val gon = 18
 
-  private var vec = (0 to gon).map { i =>
+  private val initVec = (0 to gon).map { i =>
     val angle = (2 * i * Math.PI) / gon
     val n = w / 2f
     PVector.fromAngle(angle.toFloat).mult(n)
   }.toList
+
+  def diagonal(ls: List[PVector]): List[(PVector, PVector)] = {
+    val head :: tail = ls
+    val ls2 = (head :: tail.reverse).reverse
+    // topLeft :: topRight :: bottomRight :: bottomLeft :: Nil
+    // topRight :: bottomRight :: bottomLeft :: topLeft :: Nil
+    ls.zip(ls2)
+  }
+
+  def f(resetFrameCount: Int): List[List[PVector]] = {
+    (0 to resetFrameCount).foldLeft(initVec :: Nil) { case (z, frameCount) =>
+      val head :: tail = z
+      diagonal(head).map { case (v1, v2) =>
+        val dir = PVector.sub(v2, v1).mult(gap)
+        PVector.add(v1, dir)
+      } :: z
+    }
+  }
 
   override def settings(): Unit = {
     size(w + pad, h + pad)
@@ -29,21 +45,10 @@ class RecurPolygon extends PApplet {
   override def draw(): Unit = {
     translate(width / 2, height / 2)
 
-    val diagonal = {
-      val head :: tail = vec
-      val vec2 = (head :: tail.reverse).reverse
-      // topLeft :: topRight :: bottomRight :: bottomLeft :: Nil
-      // topRight :: bottomRight :: bottomLeft :: topLeft :: Nil
-      vec.zip(vec2)
-    }
-
-    diagonal.foreach { case (v1, v2) =>
-      line(v1.x, v1.y, v2.x, v2.y)
-    }
-
-    vec = diagonal.map { case (v1, v2) =>
-      val dir = PVector.sub(v2, v1).mult(gap)
-      PVector.add(v1, dir)
+    f(300).reverse.take(frameCount % 300).foreach { vec =>
+      diagonal(vec).foreach { case (v1, v2) =>
+        line(v1.x, v1.y, v2.x, v2.y)
+      }
     }
   }
 }
